62e42161d5193909435634c974435018d7f0873d
Sketarius <sketarius@gmail.com>
2024-01-06 20:16:20 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Theme Parks API Middleware Service
+
+This project uses themeparks Node.js API
+https://github.com/cubehouse/themeparks
+
+This is simply a microservice so that you can use themeparks api from any environment you can make requests from.
+Currently the middleware only works with Walt Disney World parks and could change in the future.
+
+Operating Time requests look like:
+```
+http://localhost:3000/operatinghours?park=epcot&type=info
+```
+park [optional]
+-- epcot
+-- hollywoodstudios
+-- animalkingdom
+-- magickingdom
+
+type [optional]
+-- info
+-- ticketed-event
+-- operating
---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+var Themeparks = require('themeparks');
+
+const app = express();
+const port = 3000;
+
+var api = new Themeparks.DestinationsApi();
+var entitiesApi = new Themeparks.EntitiesApi();
+
+var mkEntityID = "";
+var epEntityID = "";
+var hsEntityID = "";
+var akEntityID = "";
+
+// Startup grab entity ids for all parks in WDW
+api.getDestinations().then(function(data) {
+    // Get Walt Disney World
+    const disneyworld = data.destinations.find(park => park.slug === "waltdisneyworldresort");
+
+    // Get EntityIDs for WDW parks
+    mkEntityID = disneyworld.parks.find(park => park.name === "Magic Kingdom Park").id;
+    epEntityID = disneyworld.parks.find(park => park.name === "EPCOT").id;
+    hsEntityID = disneyworld.parks.find(park => park.name === "Disney's Hollywood Studios").id;
+    akEntityID = disneyworld.parks.find(park => park.name === "Disney's Animal Kingdom Theme Park").id;
+});
+
+// Grab operating hours:
+// [park]
+// [type]
+app.get('/operatinghours', (req, res) => {
+    const url = require('url');
+    const queryObject = url.parse(req.url, true).query;
+    var parkRequested = mkEntityID;
+    var parkHourType = "all";
+
+    // If park is requested
+    if (!(queryObject["park"] === undefined)) {
+        switch(queryObject["park"]) {
+            case "epcot":
+                parkRequested = epEntityID;
+                break;
+            case "hollywoodstudios":
+                parkRequested = hsEntityID;
+                break;
+            case "animalkingdom":
+                parkRequested = akEntityID;
+                break;
+        }
+    }
+
+    // If park is requested
+    if (!(queryObject["type"] === undefined)) {
+        switch(queryObject["type"]) {
+            case "ticketed-event":
+                parkHourType = "TICKETED_EVENT";
+                break;
+            case "info":
+                parkHourType = "INFO";
+                break;
+            case "operating":
+                parkHourType = "OPERATING";
+                break;
+        }
+    }
+
+    entitiesApi.getEntityScheduleUpcoming(parkRequested).then(function(data) {
+        var operatingTimes = data;
+
+        if (!(parkHourType === "all")) {
+            operatingTimes = data.schedule.filter(x => x.type === parkHourType);
+        }
+        
+        res.json(operatingTimes);  
+    });
+});
+
+app.listen(port, () => {
+    console.log('Listening on port ' + port + '.');
+});
